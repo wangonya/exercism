114bac85e6ae334ffdf6d7aa611975a2cdfd3ca0
wangonya <kwangonya@gmail.com>
2024-03-27 17:17:11 +0300

    solve gigasecond

---
+++ go/gigasecond/gigasecond.go
@@ -0,0 +1,9 @@
+package gigasecond
+
+// import path for the time package from the standard library
+import "time"
+
+// AddGigasecond should have a comment documenting it.
+func AddGigasecond(t time.Time) time.Time {
+	return t.Add(time.Second * 1_000_000_000)
+}
