2648bd051ffc8d1770c21e93eb45710e1fc2b205
wangonya <kwangonya@gmail.com>
2024-05-08 06:35:48 +0300

    solve elons-toys

---
+++ go/elons-toys/elons_toys.go
@@ -0,0 +1,22 @@
+package elon
+
+import "fmt"
+
+func (c *Car) Drive() {
+	if c.battery >= c.batteryDrain {
+		c.battery -= c.batteryDrain
+		c.distance += c.speed
+	}
+}
+
+func (c Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", c.distance)
+}
+
+func (c Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", c.battery)
+}
+
+func (c Car) CanFinish(trackDistance int) bool {
+	return (float64(trackDistance)/float64(c.speed))*float64(c.batteryDrain) <= float64(c.battery)
+}
