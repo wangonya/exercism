fda6b3f7341b9e36cc92406061ec7624788d8162
wangonya <kwangonya@gmail.com>
2024-05-27 06:24:28 +0300

    add alternative approaches

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -15,3 +15,41 @@ func IsIsogram(word string) bool {
 	}
 	return true
 }
+
+// using strings.Count
+// func IsIsogram(phrase string) bool {
+// 	phrase = strings.ToLower(phrase)
+// 	for _, chr := range phrase {
+// 		if !unicode.IsLetter(chr) {
+// 			continue
+// 		}
+// 		if strings.Count(phrase, string(chr)) > 1 {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
+// using Bitfield
+// func IsIsogram(phrase string) bool {
+// 	theEnd := len(phrase)
+// 	var letterFlags uint32 = 0
+
+// 	for i := 0; i < theEnd; i++ {
+// 		letter := phrase[i]
+// 		if letter > 96 && letter < 123 {
+// 			if (letterFlags & (1 << (letter - 'a'))) != 0 {
+// 				return false
+// 			} else {
+// 				letterFlags |= (1 << (letter - 'a'))
+// 			}
+// 		} else if letter > 64 && letter < 91 {
+// 			if (letterFlags & (1 << (letter - 'A'))) != 0 {
+// 				return false
+// 			} else {
+// 				letterFlags |= (1 << (letter - 'A'))
+// 			}
+// 		}
+// 	}
+// 	return true
+// }
